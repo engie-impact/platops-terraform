4e57640a250b87d743bdbe7a22ae0acb65b37268
Wayne Lewalski <SP5867@advantageiq.com>
2019-11-05 10:13:23 -0500

    updating backend

--- backend.tf
+++ backend.tf
@@ -1,14 +1,3 @@
 provider "tfe" {
   token    = "${var.token}"
 }
-
-
-resource "tfe_organization" "main" {
-  name  = "engie-impact"
-  email = "platops@online.engie.com"
-}
-
-resource "tfe_workspace" "main" {
-  name         = "platops-tf-dev"
-  organization = "engie-impact"
-}
